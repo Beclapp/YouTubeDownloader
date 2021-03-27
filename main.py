394a401e34b9a790482a208b0ff02ab977bf9a13
Bennett Clapp <bennettclapp@gmail.com>
2021-03-27 02:07:45 -0400

    Removed unnecessary print.

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
 from pytube import YouTube
 from tkinter import *
-
+# Holds the two download options.
 values = {
     "Default": "1",
     "Audio-only": "2",
 }
 
+# Holds the two resolution options.
 resolution = {
     "360p": "360p",
     "720p": "720p",
 }
 
-
+# Downloads the video with the correct options.
 def download(link, option, res):
     yt = YouTube(link)
     ys = None
@@ -20,15 +21,19 @@ def download(link, option, res):
     else:
         ys = yt.streams.filter(only_audio=True).get_audio_only()
     print("Downloading " + yt.title + "...")
-    print(ys)
     ys.download()
     print("Download Complete!")
 
 
 window = Tk()
+# Holds the download selection option.
 v = StringVar(window, "1")
+
+# Holds the resolution selection option.
 v1 = StringVar(window, "1")
 window.geometry('600x600')
+
+# Creates the radio buttons for download.
 for(text, value) in values.items():
     Radiobutton(window, text=text, value=value, variable=v,
                 indicator=0, background="light blue").pack()
@@ -45,7 +50,6 @@ button = Button(
     fg="yellow",
     command=lambda: download(entry.get(), v.get(), v1.get())
 ).pack()
-
 button1 = Button(
     text="Quit",
     width=15,
@@ -54,12 +58,12 @@ button1 = Button(
     fg="yellow",
     command=lambda: exit(0)
 ).place(x=240, y=400)
-
 Label(
     text="Resolution: "
 ).pack()
+
+# Creates the radio buttons for resolution.
 for(text, value) in resolution.items():
     Radiobutton(window, text=text, value=value, variable=v1,
                 indicator=0, background="light blue").pack()
-
 window.mainloop()
