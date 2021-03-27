d838789d598298fe0776d76f112113c076d5ea55
Bennett Clapp <bennettclapp@gmail.com>
2021-03-27 01:58:11 -0400

    Updated script to support resolution choice.

--- main.py
+++ main.py
@@ -1,32 +1,65 @@
-# This is a sample Python script.
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 from pytube import YouTube
-import tkinter as tk
+from tkinter import *
+
+values = {
+    "Default": "1",
+    "Audio-only": "2",
+}
+
+resolution = {
+    "360p": "360p",
+    "720p": "720p",
+}
 
 
-def download(link):
+def download(link, option, res):
     yt = YouTube(link)
-    ys = yt.streams.get_highest_resolution()
-    print("Downloading...")
+    ys = None
+    if option == '1':
+        ys = yt.streams.filter(res=res, progressive='True').first()
+    else:
+        ys = yt.streams.filter(only_audio=True).get_audio_only()
+    print("Downloading " + yt.title + "...")
+    print(ys)
     ys.download()
-    print("Download completed")
+    print("Download Complete!")
 
 
-window = tk.Tk()
-label = tk.Label(
-    text="input link to download")
-entry = tk.Entry()
-button = tk.Button(
+window = Tk()
+v = StringVar(window, "1")
+v1 = StringVar(window, "1")
+window.geometry('600x600')
+for(text, value) in values.items():
+    Radiobutton(window, text=text, value=value, variable=v,
+                indicator=0, background="light blue").pack()
+Label(
+    text="Input link to download"
+).pack()
+entry = Entry(width=75)
+entry.pack()
+button = Button(
     text="Download",
     width=25,
     height=5,
     bg="blue",
     fg="yellow",
-    command=lambda : download(entry.get())
-)
-label.pack()
-entry.pack()
-button.pack()
-window.mainloop()
+    command=lambda: download(entry.get(), v.get(), v1.get())
+).pack()
 
+button1 = Button(
+    text="Quit",
+    width=15,
+    height=2,
+    bg="blue",
+    fg="yellow",
+    command=lambda: exit(0)
+).place(x=240, y=400)
+
+Label(
+    text="Resolution: "
+).pack()
+for(text, value) in resolution.items():
+    Radiobutton(window, text=text, value=value, variable=v1,
+                indicator=0, background="light blue").pack()
+
+window.mainloop()
